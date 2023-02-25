05b727f7221336fd9e1ca61fa2166b10fea2dfaf
nathanieladiah <nathanieladiah@gmail.com>
2023-02-25 15:22:19 -0400

    Add code to check for winner
    Changed the GameBoard to a 1d array to make winning check easier.
    
    If win condition is met, don't switch players and output a message.

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ const GameBoard = (() => {
   const _board = [];
 
   // 2D array to represent the state of the game board
-  for (let i = 0; i < _rows; i++) {
-    _board[i] = [];
-    for (let j = 0; j < _columns; j++) {
-      // Might use Cell or something else
-      _board[i].push(Cell());
-    }
+  //   for (let i = 0; i < _rows; i++) {
+  //     _board[i] = [];
+  //     for (let j = 0; j < _columns; j++) {
+  //       // Might use Cell or something else
+  //       _board[i].push(Cell());
+  //     }
+  //   }
+
+  // Use a 1D array instead
+  for (let i = 0; i < 9; i++) {
+    _board.push(Cell());
   }
 
   const getBoard = () => _board;
@@ -20,17 +25,23 @@ const GameBoard = (() => {
   // square should be a tuple of 2 coordinates
   const makeMove = (square, player) => {
     // If the square is not empty then return
-    if (_board[square[0]][square[1]].getValue() != null) {
+    if (_board[square].getValue() != null) {
       return 1;
     }
 
     // Otherwise the valid square.
-    _board[square[0]][square[1]].addToken(player);
+    _board[square].addToken(player);
     return 0;
   };
 
   const printBoard = () => {
-    const boardWithValues = _board.map((row) =>
+    // Convert the 1D array into a 2D array for printing
+    const oneDBoard = [..._board];
+    const twoDBoard = [];
+    while (oneDBoard.length) twoDBoard.push(oneDBoard.splice(0, 3));
+
+    // Print the values of the cells
+    const boardWithValues = twoDBoard.map((row) =>
       row.map((cell) => cell.getValue())
     );
     console.log(boardWithValues);
@@ -87,15 +98,53 @@ const GameController = ((
     console.log(`${getActivePlayer().name}'s turn`);
   };
 
+  const _checkWinner = () => {
+    // Returns true if the game has a winner
+
+    // The indexes that need to be equal for a winner
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    // let roundWon = false;
+    _board = GameBoard.getBoard();
+
+    for (let i = 0; i < 8; i++) {
+      const winCondition = winningConditions[i];
+
+      let a = _board[winCondition[0]].getValue();
+      let b = _board[winCondition[1]].getValue();
+      let c = _board[winCondition[2]].getValue();
+
+      if (a === null || b === null || c === null) {
+        continue;
+      }
+
+      if (a === b && c === b) {
+        return true;
+      }
+    }
+  };
+
   const playRound = (square) => {
     // Add token for the current player
     console.log(`Adding ${getActivePlayer().name}'s token to square ${square}`);
     if (GameBoard.makeMove(square, getActivePlayer().token) === 0) {
       // Check for winner
-
-      // Switch player turn
-      _switchPlayerTurn();
-      _printNewRound();
+      if (_checkWinner()) {
+        console.log(`Winner: ${getActivePlayer()}.`);
+      } else {
+        // Switch player turn
+        _switchPlayerTurn();
+        _printNewRound();
+      }
     } else {
       console.log(`Invalid square`);
       _printNewRound();
